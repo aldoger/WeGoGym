31980a54c0fcd3ef8d9fce97c64ee4bb5462fa49
Geraldo Benjamin Nainggolan <nainggolanben12@gmail.com>
2025-06-21 11:30:52 +0700

    feat add TODO membership

--- internal/domain/repository/membership_repository.go
+++ internal/domain/repository/membership_repository.go
@@ -1,3 +1,9 @@
 package repository
 
-//TODO Create
+//TODO Create membership
+
+//TODO read membership (ambil semua)
+
+//TODO read membership by Id
+
+//TODO update membership by id dengan harga atau durasi yang di update
